2f40b502e6afbe0f1c17121e51ac262369622914
Abhishek Amralkar <abhishekamralkar@gmail.com>
2024-07-23 15:25:59 -0700

    day 1 of python list, disctionary and loop

---
+++ m01_basics/l_01_device.py
@@ -0,0 +1,5 @@
+persons = [{"name": "anay", "location": "UK", "handsome": True, "intelligent": "maybe"},
+           {"name": "vinu", "location": "USA", "handsome": False, "intelligent": "yes"},
+           {"name": "abhi", "location": "US, CA", "handsome": True, "intelligent": "yes"}]
+for person in persons:
+    print(f"the current name is {person}")
