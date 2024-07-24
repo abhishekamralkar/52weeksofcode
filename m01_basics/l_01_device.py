7553cbf58cabecdf34ae7205480aa9314005ec4a
Abhishek Amralkar <abhishekamralkar@gmail.com>
2024-07-23 17:11:37 -0700

    day 2: introduction to forloop

--- m01_basics/l_01_device.py
+++ m01_basics/l_01_device.py
@@ -1,5 +1,25 @@
-persons = [{"name": "anay", "location": "UK", "handsome": True, "intelligent": "maybe"},
-           {"name": "vinu", "location": "USA", "handsome": False, "intelligent": "yes"},
-           {"name": "abhi", "location": "US, CA", "handsome": True, "intelligent": "yes"}]
-for person in persons:
-    print(f"the current name is {person}")
+from pprint import pprint
+
+# Dictionary representing a device
+device = {
+    "name": "sbx-n9kv-ao",
+    "vendor": "cisco",
+    "model": "Nexus9000 C9300v Chassis",
+    "os": "nsox",
+    "version": "9.3.(3)",
+    "ip": "10.1.1.1"
+}
+
+# Simple Print
+print("\n_____ SIMPLE PRINT ____________________")
+print("device: ",  device)
+print("device name:", device["name"])
+
+# PRETTY Print
+print("\n_____ PRETTY PRINT ____________________")
+pprint(device)
+
+# FOR LOOP, NICELY FORMATTED PRINT
+print("\n_______ FOR LOOP, USING F-STRING ______")
+for key, value in device.items():
+    print(f"{key:>16s} : {value}")
